2bd53cacb0bf083a818ad5b661b58a6ea214c843
Justin Jose <113986369+justinjose85@users.noreply.github.com>
2022-10-17 18:58:12 +0200

    Add files via upload

---
+++ DBConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace IntraModule
+{
+    public class DBConnection
+    {
+        public string ConnectionString = @"Server=localhost,Authentication=Windows Authentication, Database=Saturn"
+;
+        SqlConnection objConnection;
+
+        public void openConnection()
+        {
+            objConnection = new SqlConnection(ConnectionString);
+            objConnection.Open();
+        }
+
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, objConnection);
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, objConnection);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public void closeConnection()
+        {
+            objConnection.Close();
+        }
+
+
+
+    }
+}
